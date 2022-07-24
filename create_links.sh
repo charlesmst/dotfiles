ee0d2fab8220ee0b6393eb5d03152dbd76ce2ff6
Charles Stein <charles.stein@bitso.com>
2022-07-24 16:13:13 -0300

    mac specific config

--- create_links.sh
+++ create_links.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
+set -e
+
 ln -fs $(pwd)/.vimrc  $HOME/.vimrc
 ln -fs $(pwd)/.ideavimrc  $HOME/.ideavimrc
+
+if [ -d "$HOME/.config/nvim" ];then
+	ln -fs $(pwd)/.config/nvim/init.vim $HOME/.config/nvim/init.vim
+	ln -fs $(pwd)/.config/nvim/coc-settings.json $HOME/.config/nvim/coc-settings.json
+fi
+
+if [ -d "$HOME/.config/karabiner/assets/complex_modifications/" ]; then
+	ln -fs $(pwd)/.config/karabiner/assets/complex_modifications/capslock_charles.json  $HOME/.config/karabiner/assets/complex_modifications/capslock_charles.json
+	ln -fs $(pwd)/.config/karabiner/assets/complex_modifications/ctrl_enchanced.json  $HOME/.config/karabiner/assets/complex_modifications/ctrl_enchanced.json
+
+fi
+
