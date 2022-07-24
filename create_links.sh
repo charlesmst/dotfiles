2513d1c9716c91491bdaf71ed2402a85a67895db
Charles Stein <charles.stein@bitso.com>
2022-07-24 16:51:10 -0300

    add iterm config

--- create_links.sh
+++ create_links.sh
@@ -21,3 +21,8 @@ fi
 
 # Requires oh my tmux
 ln -fs "$(pwd)/.tmux.conf.local"  "$HOME/.tmux.conf.local"
+
+# Specify the preferences directory
+defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$(pwd)/macos/iTerm/settings"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
