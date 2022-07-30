26f71d44789016b9eda60248671462078c1ecc29
Charles Stein <charles.mst@gmail.com>
2022-07-30 16:25:57 +0000

    filter macos on installer

--- create_links.sh
+++ create_links.sh
@@ -22,7 +22,10 @@ fi
 # Requires oh my tmux
 ln -fs "$(pwd)/.tmux.conf.local"  "$HOME/.tmux.conf.local"
 
-# Specify the preferences directory
-defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$(pwd)/macos/iTerm/settings"
-# Tell iTerm2 to use the custom preferences in the directory
-defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	# Specify the preferences directory
+	defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$(pwd)/macos/iTerm/settings"
+	# Tell iTerm2 to use the custom preferences in the directory
+	defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
+fi
