7b2813adaeee0df5c4c3b730c2b5f73c1b860db2
Charles Stein <charles.mst@gmail.com>
2022-07-30 16:38:12 +0000

    update arch install

--- install.sh
+++ install.sh
@@ -8,15 +8,16 @@ cd yay-git/
 makepkg -si
 
 
-sofwares=tmux google-chrome slack-desktop asdf-vm gnome-shell-extension-pop-shell zsh lazygit
+sofwares=tmux google-chrome slack-desktop asdf-vm gnome-shell-extension-pop-shell zsh lazygit neovim thefuck kubectl
 
+yay -Sy $softwares
 
 # Docker desktop
 wget "https://desktop.docker.com/linux/main/amd64/docker-desktop-4.10.1-x86_64.pkg.tar.zst"
 sudo pacman -U "docker-desktop-4.10.1-x86_64.pkg.tar.zst"
 
 # pop os shell
-sudo pacman -S git typescript make
+sudo pacman -Sy git typescript make
 git clone https://github.com/pop-os/shell.git
 cd shell
 make local-install
@@ -44,3 +45,9 @@ gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 '["<Super>1
 
 # change default shell
 chsh -s $(which zsh)
+
+# oh my zsh
+git clone https://github.com/ohmyzsh/ohmyzsh ~/.oh-my-zsh
+# vim plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
