b4d083341a3b888e67c489af9f10de3f5aa95b93
Charles Stein <charles.stein@bitso.com>
2022-07-31 09:58:17 -0300

    move config local folder

--- create_links.sh
+++ create_links.sh
@@ -3,19 +3,19 @@
 set -e
 
 ln -fs $(pwd)/.zshrc  $HOME/.zshrc
-ln -fs $(pwd)/.config/zsh $HOME/.config/zsh
+ln -fs $(pwd)/config/zsh $HOME/.config/zsh
 
 ln -fs $(pwd)/.vimrc  $HOME/.vimrc
 ln -fs $(pwd)/.ideavimrc  $HOME/.ideavimrc
 
 if [ -d "$HOME/.config/nvim" ];then
-	ln -fs $(pwd)/.config/nvim/init.vim $HOME/.config/nvim/init.vim
-	ln -fs $(pwd)/.config/nvim/coc-settings.json $HOME/.config/nvim/coc-settings.json
+	ln -fs $(pwd)/config/nvim/init.vim $HOME/.config/nvim/init.vim
+	ln -fs $(pwd)/config/nvim/coc-settings.json $HOME/.config/nvim/coc-settings.json
 fi
 
 if [ -d "$HOME/.config/karabiner/assets/complex_modifications/" ]; then
-	ln -fs $(pwd)/.config/karabiner/assets/complex_modifications/capslock_charles.json  $HOME/.config/karabiner/assets/complex_modifications/capslock_charles.json
-	ln -fs $(pwd)/.config/karabiner/assets/complex_modifications/ctrl_enchanced.json  $HOME/.config/karabiner/assets/complex_modifications/ctrl_enchanced.json
+	ln -fs $(pwd)/config/karabiner/assets/complex_modifications/capslock_charles.json  $HOME/.config/karabiner/assets/complex_modifications/capslock_charles.json
+	ln -fs $(pwd)/config/karabiner/assets/complex_modifications/ctrl_enchanced.json  $HOME/.config/karabiner/assets/complex_modifications/ctrl_enchanced.json
 
 fi
 
