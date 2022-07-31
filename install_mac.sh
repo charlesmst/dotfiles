a08fb40e97d05d9123da92253addefbff1c74cd6
Charles Stein <charles.mst@gmail.com>
2022-07-31 11:53:34 -0300

    fix font used in mac

--- install_mac.sh
+++ install_mac.sh
@@ -3,7 +3,7 @@
 brew bundle install --file "./Brewfile" --verbose
 
 brew tap homebrew/cask-fonts
-brew install --cask font-aurulent-sans-mono-nerd-font 
+brew install --cask font-jetbrains-mono-nerd-font
 
 # Specify the preferences directory
 defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$(pwd)/macos/iTerm/settings"
