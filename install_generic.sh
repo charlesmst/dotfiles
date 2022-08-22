73496b9a7049ccd7879083bd5c47f0f7f68649c5
Charles Stein <charles.mst@gmail.com>
2022-08-22 18:05:51 -0300

    initial config for vim config in lua

--- install_generic.sh
+++ install_generic.sh
@@ -4,10 +4,14 @@
 git clone https://github.com/gpakosz/.tmux.git ~/.tmux
 ln -s -f ~/.tmux/.tmux.conf ~/.tmux.conf
 
+# @deprecated
 # vim plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+# vim pq
+git clone --depth=1 https://github.com/savq/paq-nvim.git \
+    "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/paqs/start/paq-nvim
 ./create_links.sh
 ./asdf.sh
 
