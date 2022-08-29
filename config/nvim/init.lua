b529a1f23be48add8a11b33d5426266cbfb2b973
Charles Stein <charles.mst@gmail.com>
2022-08-28 23:10:45 -0300

    add tmux navigator

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -30,6 +30,7 @@ require('packer').startup(function(use)
   use { 'kyazdani42/nvim-tree.lua', requires = { 'kyazdani42/nvim-web-devicons' } }
   use { 'kylechui/nvim-surround' }
   use 'overcache/NeoSolarized'
+  use 'christoomey/vim-tmux-navigator'
 
   if is_bootstrap then
     require('packer').sync()
