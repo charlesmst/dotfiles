29c55e319c5493f23411abfc7d4165e21bbab4c7
Charles Stein <charles.mst@gmail.com>
2022-10-06 23:22:03 -0300

    fix flash script

--- lily58/flash.sh
+++ lily58/flash.sh
@@ -15,5 +15,6 @@ cp charles_keymap.json ~/qmk_firmware/keyboards/lily58/keymaps/charles/
 cp rules.mk ~/qmk_firmware/keyboards/lily58/keymaps/charles/
 cd ~/qmk_firmware/keyboards/lily58/keymaps/charles
 qmk json2c charles_keymap.json > keymap.c
+cd ~/qmk_firmware
 make clean
 make lily58:charles:flash
