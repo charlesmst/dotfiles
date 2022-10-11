b7b8b08fe6192a08313af1b4df9829c57ee5ccf7
Charles Stein <charles.mst@gmail.com>
2022-10-11 12:42:16 -0300

    add usefull functions for kubernetes

--- config/zsh/zsh_alias.zsh
+++ config/zsh/zsh_alias.zsh
@@ -10,3 +10,26 @@ alias ls="ls --color"
 alias ts="~/personal/dotfiles/tmux/tmux-sessionizer.sh"
 alias history="history 1"
 alias fdocker="docker kill $(docker ps -qa) ; docker rm $(docker ps -qa)"
+
+function kclogs(){
+    if [[ -z "$1" ]]; then
+        selected=`kubectl get pods --no-headers | fzf | awk '{print $1}'`
+    else
+        selected=`kubectl get po -l app=$1 -o name `
+    fi
+    echo "selected pod $selected"
+
+    kubectl logs $selected -f
+}
+
+
+function kcbash(){
+    if [[ -z "$1" ]]; then
+        selected=`kubectl get pods --no-headers | fzf | awk '{print $1}'`
+    else
+        selected=`kubectl get po -l app=$1 -o name `
+    fi
+    echo "selected pod $selected"
+
+    kubectl exec -it $selected bash
+}
