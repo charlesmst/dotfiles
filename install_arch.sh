86f6a52c6b5fabeaa20d5b171a38e2573ff3f044
Charles Stein <charles.mst@gmail.com>
2022-11-21 18:37:02 -0300

    add ripgrep to arch installation for telescope

--- install_arch.sh
+++ install_arch.sh
@@ -12,7 +12,7 @@ if [ ! -d "$HOME/yay-git/" ]; then
 fi
 
 pacman_software="git make typescript xclip a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore fzf tmux zsh bat unzip"
-yay_sofware="asdf-vm lazygit lazydocker neovim thefuck pgcli  github-cli k9s"
+yay_sofware="asdf-vm lazygit lazydocker neovim thefuck pgcli  github-cli k9s ripgrep"
 yay_noncli="google-chrome slack-desktop intellij-idea-community-edition gnome-terminal-transparency extension-manager spotify authy docker docker-compose"
 
 echo "installing $pacman_software with pacman"
