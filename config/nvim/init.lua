9586b6c30144921b2802696dcb4719a51c75b38f
Charles Stein <charles.mst@gmail.com>
2022-11-21 19:36:13 -0300

    fix transparent theme tokyonight

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -97,7 +97,6 @@ vim.wo.signcolumn = 'yes'
 
 -- Set colorscheme
 vim.o.termguicolors = true
-vim.cmd [[colorscheme tokyonight-storm]]
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
