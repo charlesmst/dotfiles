5cf06eca5dd2d97934e881e47929f1940de9b559
Charles Stein <charles.mst@gmail.com>
2023-02-07 17:12:10 -0300

    add tri layer to flash

--- lily58/flash.sh
+++ lily58/flash.sh
@@ -12,9 +12,11 @@ sudo cp -f ~/qmk_firmware/util/udev/50-qmk.rules /etc/udev/rules.d/
 rm -rf ~/qmk_firmware/keyboards/lily58/keymaps/charles/
 mkdir -p ~/qmk_firmware/keyboards/lily58/keymaps/charles
 cp charles_keymap.json ~/qmk_firmware/keyboards/lily58/keymaps/charles/
+cp custom.c ~/qmk_firmware/keyboards/lily58/keymaps/charles/
 cp rules.mk ~/qmk_firmware/keyboards/lily58/keymaps/charles/
 cd ~/qmk_firmware/keyboards/lily58/keymaps/charles
 qmk json2c charles_keymap.json > keymap.c
+cat custom.c >> keymap.c
 cd ~/qmk_firmware
 make clean
 make lily58:charles:flash
