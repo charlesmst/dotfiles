8416fac76cd2ab21ba921329503afd12f932f02e
Charles Stein <charles.mst@gmail.com>
2023-02-27 10:02:07 -0300

    disable tree sitter highlight

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -194,7 +194,7 @@ require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
   ensure_installed = { 'lua', 'typescript', 'rust', 'go', 'python', 'java', 'swift', 'hcl', 'dart', 'c'},
 
-  highlight = { enable = true },
+  highlight = { enable = false },
   indent = { enable = true },
   incremental_selection = {
     enable = true,
