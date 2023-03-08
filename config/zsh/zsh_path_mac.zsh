e9e7846333e68348b98b99df142317d93af367fd
Charles Stein <charles.mst@gmail.com>
2023-03-08 12:44:19 -0300

    fix asdf path on mac

--- config/zsh/zsh_path_mac.zsh
+++ config/zsh/zsh_path_mac.zsh
@@ -4,7 +4,7 @@ if [ -d "/opt/homebrew/opt/libpq/bin" ];then
 	export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
 fi
 
-. /opt/homebrew/opt/asdf/asdf.sh
+. "$(brew --prefix asdf)/libexec/asdf.sh"
 
 export IDEA=/Applications/IntelliJ\ IDEA\ CE.app/Contents/MacOS/idea
 
