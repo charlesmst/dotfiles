55f685dd866c61bdf954f9481d95784a14a778f8
Charles Stein <charles.mst@gmail.com>
2023-03-11 22:33:08 -0300

    migrate packer to lazy

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,83 +1,56 @@
--- Install packer
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-local is_bootstrap = false
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  is_bootstrap = true
-  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
-  vim.cmd [[packadd packer.nvim]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
--- stylua: ignore start
-require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'                                                    -- Package manager
-  use 'tpope/vim-fugitive'                                                        -- Git commands in nvim
-  use 'tpope/vim-rhubarb'                                                         -- Fugitive-companion to interact with github
-  use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }       -- Add git related info in the signs columns and popups
-  use 'numToStr/Comment.nvim'                                                     -- "gc" to comment visual regions/lines
-  use 'nvim-treesitter/nvim-treesitter'                                           -- Highlight, edit, and navigate code
-  use 'nvim-treesitter/nvim-treesitter-textobjects'                               -- Additional textobjects for treesitter
-  -- use 'neovim/nvim-lspconfig'                                                     -- Collection of configurations for built-in LSP client
-  -- use 'williamboman/nvim-lsp-installer'                                           -- Automatically install language servers to stdpath
-  --
-  --
-  -- MASON
-  use {
+
+vim.g.mapleader = ' '
+require('lazy').setup({
+  'tpope/vim-fugitive',
+  'tpope/vim-rhubarb',                                                        
+  { 'lewis6991/gitsigns.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
+  'numToStr/Comment.nvim',
+  'nvim-treesitter/nvim-treesitter',
+  'nvim-treesitter/nvim-treesitter-textobjects',
+  {
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
-  }
-  use { 'hrsh7th/nvim-cmp', requires = { 'hrsh7th/cmp-nvim-lsp' } }               -- Autocompletion
-  use { 'L3MON4D3/LuaSnip', requires = { 'saadparwaiz1/cmp_luasnip' } }           -- Snippet Engine and Snippet Expansion
-  use 'nvim-lualine/lualine.nvim'                                                 -- Fancier statusline
-  use 'tpope/vim-sleuth'                                                          -- Detect tabstop and shiftwidth automatically
-  use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } } -- Fuzzy Finder (files, lsp, etc)
+  },
+   { 'hrsh7th/nvim-cmp', dependencies = { 'hrsh7th/cmp-nvim-lsp' } },
+   { 'L3MON4D3/LuaSnip', dependencies = { 'saadparwaiz1/cmp_luasnip' } },
+   'nvim-lualine/lualine.nvim',
+   'tpope/vim-sleuth',
+   { 'nvim-telescope/telescope.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
 
-  -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable "make" == 1 }
+  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make', cond = vim.fn.executable "make" == 1 },
 
-  use 'lukas-reineke/indent-blankline.nvim'
-  use { 'kyazdani42/nvim-tree.lua', requires = { 'kyazdani42/nvim-web-devicons' } }
-  use { 'kylechui/nvim-surround' }
-  use 'folke/tokyonight.nvim'
-  use 'christoomey/vim-tmux-navigator'
+   'lukas-reineke/indent-blankline.nvim',
+   { 'kyazdani42/nvim-tree.lua', dependencies = { 'kyazdani42/nvim-web-devicons' } },
+   { 'kylechui/nvim-surround' },
+   'folke/tokyonight.nvim',
+   'christoomey/vim-tmux-navigator',
 
   -- Debug
-  use { 'mfussenegger/nvim-dap' }
-  use { 'nvim-telescope/telescope-dap.nvim' }
-  use { 'mfussenegger/nvim-dap-python' } 
-  use { 'leoluz/nvim-dap-go' }
-  use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
+   { 'mfussenegger/nvim-dap' },
+   { 'nvim-telescope/telescope-dap.nvim' },
+   { 'mfussenegger/nvim-dap-python' } ,
+   { 'leoluz/nvim-dap-go' },
+   { "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap"} },
 
-  use { 'aklt/plantuml-syntax' }
-  use { "windwp/nvim-autopairs" }
-  use { 'tpope/vim-obsession'}
+   { 'aklt/plantuml-syntax' },
+   { "windwp/nvim-autopairs" },
+   { 'tpope/vim-obsession'}
 
-  if is_bootstrap then
-    require('packer').sync()
-  end
-end)
--- stylua: ignore end
-
--- When we are bootstrapping a configuration, it doesn't
--- make sense to execute the rest of the init.lua.
---
--- You'll need to restart nvim, and then it will work.
-if is_bootstrap then
-  print '=================================='
-  print '    Plugins are being installed'
-  print '    Wait until Packer completes,'
-  print '       then restart nvim'
-  print '=================================='
-  return
-end
-
--- Automatically source and re-compile packer whenever you save this init.lua
-local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePost', {
-  command = 'source <afile> | PackerCompile',
-  group = packer_group,
-  pattern = vim.fn.expand '$MYVIMRC',
-})
+  })
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -111,11 +84,6 @@ vim.o.termguicolors = true
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
 
--- [[ Basic Keymaps ]]
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.nvim_tree_respect_buf_cwd = 1
 vim.o.incsearch = true
