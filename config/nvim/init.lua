629afcfc7705ec5bf1fecbaad0c7470847b690a7
Charles Stein <charles.mst@gmail.com>
2023-03-19 19:56:23 -0300

    add dap

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -72,4 +72,22 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 
-require("charles")
+
+
+vim.keymap.set('n', '<leader>w', ":w<CR>")
+vim.keymap.set('n', '<leader>q', ":q<CR>")
+vim.keymap.set({'i', 'v'}, 'kj', "<esc>")
+
+vim.keymap.set('n', '<leader>e', ":NvimTreeFindFileToggle<CR>")
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+
+
+vim.keymap.set('n', '<C-d>', "<C-d>zz")
+vim.keymap.set('n', '<C-u>', "<C-u>zz")
+
+vim.keymap.set('n', 'n', "nzzzv")
+vim.keymap.set('n', 'N', "Nzzzv")
+
--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -1,24 +1,9 @@
 return {
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
-  {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-  },
   'tpope/vim-sleuth',
-
   'lukas-reineke/indent-blankline.nvim',
-  'folke/tokyonight.nvim',
   'christoomey/vim-tmux-navigator',
-
-  -- Debug
-  { 'mfussenegger/nvim-dap' },
-  { 'nvim-telescope/telescope-dap.nvim' },
-  { 'mfussenegger/nvim-dap-python' },
-  { 'leoluz/nvim-dap-go' },
-  { "rcarriga/nvim-dap-ui",             dependencies = { "mfussenegger/nvim-dap" } },
-
   { 'aklt/plantuml-syntax' },
   { 'tpope/vim-obsession' }
 
