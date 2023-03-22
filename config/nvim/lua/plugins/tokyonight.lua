cb68538236d3c69da8cba6d6257ae507e2e8f40e
Charles Stein <charles@CHARLES-PC.localdomain>
2023-03-21 22:07:26 -0300

    disable transparency on wsl

--- config/nvim/lua/plugins/tokyonight.lua
+++ config/nvim/lua/plugins/tokyonight.lua
@@ -1,12 +1,16 @@
 return {
   'folke/tokyonight.nvim',
   config = function()
+    local is_wsl = (function() 
+      local output = vim.fn.systemlist "uname -r" 
+      return not not string.find(output[1] or "", "WSL")
+    end)() 
     require("tokyonight").setup({
       -- your configuration comes here
       -- or leave it empty to use the default settings
       style = "storm",        -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
       light_style = "day",    -- The theme is used when the background is set to light
-      transparent = true,     -- Enable this to disable setting the background color
+      transparent = not is_wsl,     -- Enable this to disable setting the background color
       terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
       styles = {
         -- Style to be applied to different syntax groups
