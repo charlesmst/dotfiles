98db10bd194e589f48324ec4bca09a819522860d
Charles Stein <charles.mst@gmail.com>
2023-08-18 11:18:07 -0300

    use custom font in alacrity

--- install_arch.sh
+++ install_arch.sh
@@ -13,7 +13,7 @@ fi
 
 pacman_software="git make typescript xclip a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore fzf tmux zsh bat unzip xdg-utils jq"
 yay_sofware="asdf-vm lazygit lazydocker neovim thefuck pgcli  github-cli k9s ripgrep"
-yay_noncli="google-chrome slack-desktop intellij-idea-community-edition gnome-terminal-transparency extension-manager spotify authy docker docker-compose"
+yay_noncli="google-chrome slack-desktop intellij-idea-community-edition gnome-terminal-transparency extension-manager spotify authy docker docker-compose ttf-firacode-nerd ttf-sourcecodepro-nerd"
 
 echo "installing $pacman_software with pacman"
 sudo pacman -Sy $pacman_software
@@ -26,9 +26,6 @@ yay -Sy $yay_software
 chsh -s $(which zsh)
 
 if [ -n "$DESKTOP_SESSION" ]; then
-	echo "INSTALLING CLI PACKAGES"
-	echo "installing fonts"
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
 
 	echo "installing $yay_noncli"
 	yay -Sy $yay_noncli
@@ -39,9 +36,6 @@ if [ -n "$DESKTOP_SESSION" ]; then
 
 	echo "installing flatpacks"
 	flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-
-	# enable dark theme chrome
-	bash -c "$(curl -fsSL "https://raw.githubusercontent.com/felipecassiors/dotfiles/master/scripts/enable_chrome_dark_mode.sh")"
 
 	# change gnome settings for multiple workspaces
 	gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-9 '["<Control><Shift><Alt>9"]'
@@ -68,16 +62,6 @@ if [ -n "$DESKTOP_SESSION" ]; then
 
 	gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['f2', 'XF86Keyboard']"
 	# gsettings set org.gnome.shell.keybindings toggle-application-view "['<Super>space']"
-
-	# pop os shell
-	if [ ! -d "$HOME/shell/" ]; then
-		cd ~/
-		git clone https://github.com/pop-os/shell.git
-		cd shell
-		make local-install
-		cd ~/
-		gnome-extensions enable pop-shell@system76.com
-	fi
 fi
 
 
