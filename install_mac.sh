98f3c00a5b7473a66acaccf2dbe87ce23c6bb0db
Charles Stein <charles.mst@gmail.com>
2023-10-19 10:48:14 -0300

    fix alacritty font

--- install_mac.sh
+++ install_mac.sh
@@ -5,6 +5,7 @@ brew bundle install --file "./Brewfile" --verbose
 brew tap homebrew/cask-fonts
 brew install --cask font-jetbrains-mono-nerd-font
 brew install --cask font-dejavu-sans-mono-nerd-font
+brew install --cask font-fira-code-nerd-font
 
 # Specify the preferences directory
 defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$(pwd)/macos/iTerm/settings"
