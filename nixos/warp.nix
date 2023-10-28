8c86a7b44310c980431365b4f0de4ca86d49a9f4
Charles Stein <charles.mst@gmail.com>
2023-10-28 11:54:17 -0300

    warp

---
+++ nixos/warp.nix
@@ -0,0 +1,18 @@
+
+{ config, lib, pkgs, user, ... }:
+
+{
+  # Cloudfare WARP
+  # PR: https://github.com/NixOS/nixpkgs/pull/168092
+  # Issue: https://discourse.nixos.org/t/cant-start-cloudflare-warp-cli/23267
+  imports = [
+    ./warp/cloudflare-warp.nix
+  ];
+  services = {
+    cloudflare-warp = {
+      enable = true;
+      certificate = "/home/${user}/Cloudflare_CA.crt";
+    };
+  };
+
+}
