78fbcf6fec3a56353169c9c104e3e81a5f7075cb
Charles Stein <charles.mst@gmail.com>
2023-10-27 22:12:49 -0300

    easy simple solution for i2c

--- nixos/nixos/hardware-configuration.nix
+++ nixos/nixos/hardware-configuration.nix
@@ -9,13 +9,11 @@
     ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
-  boot.initrd.kernelModules = ["i2c-dev"  ];
+  boot.initrd.kernelModules = [];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
- services.udev.extraRules = ''
-        KERNEL=="i2c-[0-9]*", GROUP="i2c", MODE="0660"
-  '';
+
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/27fd9251-edb2-4f06-a9b6-99b08c6282bc";
       fsType = "ext4";
@@ -40,4 +38,5 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.i2c.enable = true;
 }
